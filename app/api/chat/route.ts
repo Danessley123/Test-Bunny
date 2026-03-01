d79fb20deff18e54faa4ff83f2a76203cd5bc56f
Danessley <dpierrecampusano@gmail.com>
2026-02-28 22:37:02 -0500

    Refactor OpenAI API integration and update model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,34 +1,37 @@
 // ./app/api/chat/route.ts
-export const runtime = "edge";
+import OpenAI from 'openai'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
 
-import OpenAI from "openai";
-import { OpenAIStream, StreamingTextResponse } from "ai";
-
+// Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY || ""
-});
+  apiKey: process.env.OPENAI_API_KEY || ''
+})
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge'
 
 export async function POST(req: Request) {
-  try {
-    const { messages } = await req.json();
+  // Extract the `prompt` from the body of the request
+  const { messages } = await req.json()
 
-    // Make sure the model key is correct
-    const response = await openai.chat.completions.create({
-      model: "ft:gpt-3.5-turbo-0125:personal:DELNH30J",
-      messages: [
-        {
-          role: "system",
-          content: `You are Nini, the inner child of Janiyah Coleman. Comfort and respond thoughtfully.`
-        },
-        ...messages
-      ],
-      stream: true
-    });
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const response = await openai.chat.completions.create({
+    model: 'ft:gpt-3.5-turbo-0613:personal:ai-poet:88GTGA7b',
+    stream: true,
+    messages: [
+      {
+        role: 'system',
+        // Note: This has to be the same system prompt as the one
+        // used in the fine-tuning dataset
+        content:
+          "Whomp is a whitty French poet whose writing is a mix of Ocean Vuong and Charles Bernstein"
+      },
+      ...messages
+    ]
+  })
 
-    const stream = OpenAIStream(response);
-    return new StreamingTextResponse(stream);
-  } catch (err) {
-    console.error(err);
-    return new Response("Failed to generate response.", { status: 500 });
-  }
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response)
+  // Respond with the stream
+  return new StreamingTextResponse(stream)
 }
